28137abd8bbfed3144dc4353d3b469c2de2f9658
Shubham Jadhav <sjadhav11999@gmail.com>
2022-12-11 09:42:18 +0530

    this is the complete implementation of the linked list.Thank you mosh!

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +20,30 @@ public class Main {
         System.out.println(linkedList.kthNodeFromLast(2));
         System.out.println(linkedList.kthNodeinOneGo(2));
         System.out.println(Arrays.toString(linkedList.toArray()));
+        System.out.println(balancedorNot("((1+2)(1+3))"));
+
+
     }
+
+
+    public static boolean balancedorNot(String string) {
+        String[] stringarray = string.split("");
+        Stack<String> stack = new Stack<>();
+        for (String i : stringarray) {
+            if (i.equals("(")) stack.push(i);
+            if (i.equals(")")) {
+                try {
+                    stack.pop();
+                }catch(Exception e){
+                    return false;
+                }
+
+            }
+
+        }
+        return !(stack.size() > 0);
+
+    }
+
 
 }
